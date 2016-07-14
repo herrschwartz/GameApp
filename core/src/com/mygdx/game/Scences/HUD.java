0876f793285cb9ea43c22ff50d9b5705e06528e5
Tim <herrschwartz@gmail.com>
2016-07-14 14:08:27 -0700

    Initial commit

---
+++ core/src/com/mygdx/game/Scences/HUD.java
@@ -0,0 +1,56 @@
+package com.mygdx.game.Scences;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.MyGdxGame;
+
+/**
+ * Created by Tim on 6/30/2016.
+ */
+public class HUD {
+    public Stage stage;
+    private Viewport viewport;
+
+    private Integer worldTimer;
+    private float timeCount;
+    private Integer score;
+
+
+
+    public HUD(SpriteBatch sb){
+        worldTimer = 300;
+        timeCount = 0;
+        score = 0;
+
+        viewport = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, sb);
+
+        Label countDownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        Label scoreLabel= new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));;
+        Label timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        Label levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        Label worldLabel =new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        Label MarioLabel = new Label("Mario", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        table.add(MarioLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(levelLabel).expandX();
+        table.add(countDownLabel).expandX();
+
+        stage.addActor(table);
+    }
+}
---
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -0,0 +1,70 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Scences.HUD;
+
+/**
+ * Created by Tim on 6/29/2016.
+ */
+public class PlayScreen implements Screen {
+    private MyGdxGame game;
+    private OrthographicCamera gamecam;
+    private Viewport gameport;
+    Texture texture;
+    private HUD hud;
+
+    public PlayScreen(MyGdxGame game){
+        this.game = game;
+        gamecam = new OrthographicCamera();
+        gameport = new FitViewport(MyGdxGame.V_WIDTH,MyGdxGame.V_HEIGHT, gamecam);
+        hud = new HUD(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gameport.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
